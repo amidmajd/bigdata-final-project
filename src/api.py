96ec765499cc9ba44b6386574b0ca04103a8c220
Amid Majd <amidmajd@gmail.com>
2022-07-19 14:23:05 +0430

    fix CORS origins

--- src/api.py
+++ src/api.py
@@ -1,12 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .api_routes import router
-
 
 BASE_URL = "/api"
 
 app = FastAPI(
     debug=True, title="Kafka API", openapi_url="/openapi.json", redoc_url=None, docs_url="/docs"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
